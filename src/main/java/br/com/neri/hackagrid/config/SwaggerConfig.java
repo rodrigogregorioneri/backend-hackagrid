4baf5892bca418ebe47955c99ab74e744fad4536
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2019-12-14 14:31:42 -0300

    conectando com MongoDB e adicionando o Swagger para as APIs

---
+++ src/main/java/br/com/neri/hackagrid/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package br.com.neri.hackagrid.config;
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.collect.Lists;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .enableUrlTemplating(false);
+    }
+
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Hacka Grid")
+                .description("Hacka Grid")
+                .version("1.0")
+                .build();
+    }
+}
