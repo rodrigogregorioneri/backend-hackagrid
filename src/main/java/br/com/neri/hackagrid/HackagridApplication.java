445fa6516c222c553f17d8950a7855ccc5f2a929
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2019-12-14 14:04:55 -0300

    subindo projeto inicial

---
+++ src/main/java/br/com/neri/hackagrid/HackagridApplication.java
@@ -0,0 +1,13 @@
+package br.com.neri.hackagrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackagridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackagridApplication.class, args);
+	}
+
+}
